4e11243f9f3e9bd2efe4842fcce12d9d303b8ddf
Andrew Stuart <andrew.stuart2@gmail.com>
2015-07-28 18:26:12 -0700

    Add eslint for tests and further testing

--- test/spec/directives/up-content-folder.js
+++ test/spec/directives/up-content-folder.js
@@ -1,22 +1,25 @@
 describe('up-content-folder directive', function() {
+    'use strict';
 
-    function testHandler ($scope) {
+
+    var testHandler = jasmine.createSpy('testHandler').and.callFake(function($scope) {
         $scope.hello = $scope.def.foo;
-    }
+    });
 
-    function barHandler ($scope) {
+    var barHandler = jasmine.createSpy('barHandler').and.callFake(function ($scope, iEle) {
+        iEle.attr('foo', 'bar');
         $scope.whoa = 'man';
-    }
+    });
 
-    var fakeElement = '<up-content-folder def="foo">' + 
+    var fakeElement = '<up-content-folder def="foo">' +
         '<span>{{hello}}</span>' +
-        '<up-content-folder ng-repeat="child in def.content" def="child"></up-content-folder>' + 
+        '<up-content-folder ng-repeat="child in def.content" def="child"></up-content-folder>' +
     '</up-content-folder>';
 
     var fakeDoubleEle = '<up-content-folder def="foo">{{hello}}</up-content-folder>' +
         '<up-content-folder def="bar">{{whoa}}</up-content-folder>';
 
-    var provider, folder, scope;
+    var provider, scope;
 
     beforeEach(function() {
         var fakeMod = angular.module('test.content', function() {});
@@ -32,7 +35,6 @@ describe('up-content-folder directive', function() {
         module('up-content-folder', 'test.content');
 
         inject(function(Folder, $compile, $rootScope) {
-            folder = Folder;
             scope = $rootScope.$new();
 
             scope.foo = {
@@ -54,7 +56,11 @@ describe('up-content-folder directive', function() {
             fakeDoubleEle = $compile(fakeDoubleEle)(scope);
             scope.$digest();
         });
+    });
 
+    afterEach(function() {
+        barHandler.calls.reset();
+        testHandler.calls.reset();
     });
 
     describe('Compilation of up-content-folder', function() {
@@ -67,6 +73,11 @@ describe('up-content-folder directive', function() {
     describe('double element compilation', function() {
         it('should use each root element\'s own contained template', function() {
             expect(fakeDoubleEle.text()).toEqual('barman');
+            expect(fakeDoubleEle.eq(1).attr('foo')).toEqual('bar');
+        });
+
+        it('should expose some child $scope as the first argument to handler functions', function() {
+            expect(barHandler.calls.argsFor(0)[0].$parent).toBe(scope);
         });
     });
 });
