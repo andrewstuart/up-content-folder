7fafc385387e1cb03cdf1fc68366dcff4b8d9bd3
Andrew Stuart <andrew.stuart2@gmail.com>
2015-07-29 09:20:53 -0700

    Use a plain child scope rather than isolate.

--- dist/up-content-folder.js
+++ dist/up-content-folder.js
@@ -4,7 +4,8 @@ angular.module('up-content-folder', ['RecursionHelper']);
  * @ngdoc directive
  * @name up-content-folder.directive#upContentFolder
  * @description A container for either portlets or (recursively) other containers
- * @param {Object} def The display
+ * @param {Object} def The display definition, linked bidirectionally.
+ * @scope
  * @restrict E
  */
 
@@ -13,15 +14,22 @@ angular.module('up-content-folder')
         'use strict';
 
         return {
-            scope: {
-                def: '='
-            },
+            //Rather than an isolate scope, use a plain child scope.
+            //This allows more-intuitive use of the given template
+            scope: true,
             restrict: 'E',
             compile: function(tEle) {
                 var tmpl = tEle.html();
                 tEle.html('');
 
                 return RecursionHelper.compile(tEle, function link($scope, iEle, iAttrs) {
+
+                    //Manually add def to the scope and watch it for updates.
+                    $scope.def = $scope.$eval(iAttrs.def);
+                    $scope.$watch(iAttrs.def, function(d) {
+                        $scope.def = d;
+                    });
+
                     //TODO find a better place than the scope to store this.
                     $scope.$$$tmpl = $scope.$parent.$$$tmpl || tmpl;
 
